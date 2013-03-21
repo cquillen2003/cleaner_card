cd7c417f2e217de277bfdf4d196839da5aef8767
Corey Quillen <corey.quillen@gmail.com>
2013-03-20 22:42:57 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CleanerCard::Application.config.secret_token = 'e1dd24edd726cc443942e868d36089fc4fa4a00a389c44c851ef09824df3d4bd0829e948c32c8321b1a4afbcbf7986f8da62fc95af2dc434ec69253d119b4d1f'
