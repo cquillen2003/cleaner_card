ce9959b702e79548895abb054a10521c9fed9949
Corey Quillen <corey.quillen@gmail.com>
2013-04-11 00:35:26 -0500

    fix ajax response bug

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -1,5 +1,5 @@
 
-
+//drag and drop accross columns
 	$(function() {
 		$( ".columns" ).sortable({
 				connectWith: ".columns"
@@ -7,3 +7,21 @@
 		$( ".columns" ).disableSelection();
 	});
 
+
+//Stying and interaction for portlets
+
+$(function() {
+
+	$( ".portlet" ).addClass( "ui-widget ui-widget-content ui-helper-clearfix ui-corner-all" )
+		.find( ".portlet-header" )
+			.addClass( "ui-widget-header ui-corner-all" )
+			.prepend( "<span class='ui-icon ui-icon-minusthick'></span>")
+			.end()
+		.find( ".portlet-content" );
+ 
+	$( ".portlet-header .ui-icon" ).click(function() {
+		$( this ).toggleClass( "ui-icon-minusthick" ).toggleClass( "ui-icon-plusthick" );
+		$( this ).parents( ".portlet:first" ).find( ".portlet-content" ).toggle();
+	});
+	  
+});
